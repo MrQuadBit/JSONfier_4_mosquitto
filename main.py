51e8a53283870cf266486398d16ccc8b9341653d
dano <mrquadbit@gmail.com>
2021-07-29 15:21:44 -0500

    beta 1.0

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
-import sys
-
 def main():
 	#CLI Tool
 	item = ""
 	json = {}
 
+	#Instructions
+	print("----------------------------------------------------------------------------")
+	print("Ingresa la clave y el valor separados por un espacio, e.g.")
+	print("\nID 1234")
+	print("Nombre Fulano")
+	print("mnsg Hola mundo con JSON")
+	print("JSON ->  {'ID': '1234', 'Nombre': 'Fulano', 'mnsg': 'Hola mundo con JSON'}\n")
+	print("**Para salir ingresa la letra 'C' o 'c'**", end="\n\n")
+
+	#Getting and filling the JSON
 	while(True):
-		item = instructionsMenu2()
+		item = input()
 
 		#Check if user wants to exit
 		if(item == 'C' or item == "c"):
 			break
 
+		#Creating dictionary
 		item = item.split()
-		json[item[0]] = "".join(item[1:])
+		json[item[0]] = " ".join(item[1:])
 
-		print(json)
+		print("JSON -> ", json, end="\n\n")
 
-def instructions():
-	print("\n-----------------------------------------------------------")
-	print("Ingresa la clave y el valor separados por un espacio")
-	print("e.g.\n-->ID 1234")
-	print("**Para salir ingresa la letra 'C' o 'c'**")
-	return input()
-	print("-----------------------------------------------------------\n")
+	#Formatting string
+	print("Quieres agregar un '\\' antes de cada '\"' ?")
+	answer = input("(y/n)")
+	if answer == 'y':
+		print(str(json).replace("\'", "\\\""))
+	else:
+		print(str(json).replace("\'", "\""))
 if __name__ == "__main__":
 	main()
